cac086c671f331cfa030a4da3d8d63e14a629094
Karolina Kozubik <k4aro0@gmail.com>
2024-02-13 18:46:24 +0100

    clear

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import PlaceholderItem from './components/PlaceholderItem.vue';
+import PlaceholderItem from '@/components/PlaceholderItem.vue';
 </script>
 
 <template>
+  <header>
+  </header>
   <main class="flex justify-center min-w-full">
+    <section id="content"></section>
     <PlaceholderItem />
+    <section id="sponsors"></section>
   </main>
+  <footer>
+  </footer>
 </template>
