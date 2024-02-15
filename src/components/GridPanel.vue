e3a309be78dcb1b8964b8f2383cb6c031a650462
Karolina Kozubik <k4aro0@gmail.com>
2024-02-15 19:36:07 +0100

    add grid for main page:
    every second row gets the order of element reversed for window size from md up

---
+++ src/components/GridPanel.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { GridItem } from '@/interfaces'
+import GridRow from '@/components/GridRow.vue'
+
+defineProps<{
+  items: Array<GridItem>
+}>()
+</script>
+
+<template>
+  <div class="grid grid-cols-1 gap-4">
+    <GridRow v-for="item in items" :key="item.id" :reversed="item.id % 2 == 0">
+      <template v-slot:description>
+        <component :is="item.description" />
+      </template>
+      <template v-slot:object>
+        <component :is="item.object" />
+      </template>
+    </GridRow>
+  </div>
+</template>
