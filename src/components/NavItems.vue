89274751aa3790fa19dc3291917511cf701e7bbb
Karolina Kozubik <k4aro0@gmail.com>
2024-02-15 20:41:35 +0100

    navbar component with mobile and desktop nav

---
+++ src/components/NavItems.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { type LinkItem } from '@/interfaces'
+import { inject } from 'vue'
+
+const navItems: Array<LinkItem> = [{ name: 'home' }, { name: 'about' }]
+
+const toTitle = (name: String) =>
+  (name.charAt(0).toUpperCase() + name.slice(1)).trim().replace('/[-_]/gm', ' ')
+
+const isOpened = inject('isOpened')
+</script>
+
+<template>
+  <router-link
+    v-for="item in navItems"
+    :key="item.name"
+    :to="{ name: item.name }"
+    @click="isOpened = !isOpened"
+  >
+    {{ toTitle(item.name) }}
+  </router-link>
+</template>
