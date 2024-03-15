06b46de902f0e436396020d1c4114edc5735479e
Karolina Kozubik <k4aro0@gmail.com>
2024-03-15 21:24:01 +0100

    add PartnersPanel.vue

---
+++ src/components/PartnersPanel.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const sponsors: { path: string; alt: string }[] = []
+</script>
+
+<template>
+  <div v-if="sponsors.length" class="py-24 w-2/3 mx-auto">
+    <p class="font-bold text-center text-5xl">Partnerzy wydarzenia</p>
+    <div class="pt-16 flex flex-wrap justify-center">
+      <div v-for="sponsor in sponsors" :key="sponsors.indexOf(sponsor)">
+        <img class="h-40 m-8" :src="sponsor.path" :alt="sponsor.alt" />
+      </div>
+    </div>
+  </div>
+</template>
