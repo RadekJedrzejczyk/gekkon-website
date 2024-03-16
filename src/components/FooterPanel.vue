6b48dc8fc503186d4716ddad0ce422890640505d
Karolina Kozubik <k4aro0@gmail.com>
2024-03-16 11:32:27 +0100

    add fb to footer

--- src/components/FooterPanel.vue
+++ src/components/FooterPanel.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import NavItems from '@/components/NavItems.vue'
+import FacebookIcon from '@/components/icons/FacebookIcon.vue'
+
+const socialIconClass = 'size-8 mr-4'
 </script>
 
 <template>
@@ -8,19 +11,28 @@ import NavItems from '@/components/NavItems.vue'
       <div class="pb-8 md:pb-0">
         <p class="font-bold text-lg pb-4">Europejskie Forum Studentów AEGEE-Gliwice</p>
         <p class="pb-4">
-          Centrum Kultury Studenckiej "Mrowisko" <br>
-          ul. Pszczyńska 85 <br>
-          44-100 Gliwice <br>
+          Centrum Kultury Studenckiej "Mrowisko" <br />
+          ul. Pszczyńska 85 <br />
+          44-100 Gliwice <br />
         </p>
         <p>email: gekkon@aegee-gliwice.com</p>
       </div>
       <div class="pb-8 md:pb-0">
-        <NavItems classes="block"/>
+        <NavItems classes="block" />
       </div>
-      <div>Sociale</div>
+      <div>
+        <p class="font-bold text-lg pb-4">Odwiedź nasze sociale</p>
+        <div class="flex">
+          <a
+            href="https://www.facebook.com/profile.php?id=61556661431893"
+            target="_blank"
+            ><FacebookIcon :class="socialIconClass"
+          /></a>
+        </div>
+      </div>
     </div>
     <p class="text-center mt-8 pb-8">
-      Created with ♥ by <a href="https://github.com/karo-fox">Karolina Kozubik</a>, 2024
+      Created with ♥ by <a href="https://github.com/karo-fox" class="text-marshland-200">Karolina Kozubik</a>, 2024
     </p>
   </div>
 </template>
