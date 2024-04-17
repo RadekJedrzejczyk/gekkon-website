f709d5374616ac52908495070b290418dd97f149
Karolina Kozubik <k4aro0@gmail.com>
2024-04-17 11:35:04 +0200

    prerelease fixes

--- src/views/RegulatonsView.vue
+++ src/views/RegulatonsView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import LinkCell from '@/components/LinkCell.vue'
 
-const regulationsPDFLink = '/docs/regulamin.pdf'
+const regulationsPDFLink = '/docs/konferencja-gekkon-regulamin.pdf'
 const headerStyle = 'font-medium text-3xl pb-4'
 const linkClass =
   'bg-accent-600 text-marshland-950 p-2 text-center mx-auto border-solid rounded-lg font-bold hover:bg-accent-700'
