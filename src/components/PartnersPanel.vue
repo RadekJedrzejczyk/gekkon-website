89d8edd4e4a8a93550bdfc4eba0c96113f833abf
Karolina Kozubik <k4aro0@gmail.com>
2024-04-24 13:43:48 +0200

    add pOLSA to partners list

--- src/components/PartnersPanel.vue
+++ src/components/PartnersPanel.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-const sponsors: { path: string; alt: string }[] = []
+const sponsors: { path: string; alt: string }[] = [
+  { path: "/partners/POLSAlogotypBLACK.png", alt: "Logotyp POLSA" },
+]
 </script>
 
 <template>
@@ -7,7 +9,7 @@ const sponsors: { path: string; alt: string }[] = []
     <p class="font-bold text-center text-5xl">Partnerzy wydarzenia</p>
     <div class="pt-16 flex flex-wrap justify-center">
       <div v-for="sponsor in sponsors" :key="sponsors.indexOf(sponsor)">
-        <img class="h-40 m-8" :src="sponsor.path" :alt="sponsor.alt" />
+        <img class="h-40 my-8 md:mx-8" :src="sponsor.path" :alt="sponsor.alt" />
       </div>
     </div>
   </div>
