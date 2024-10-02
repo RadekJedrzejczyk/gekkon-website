ed135f7426399538f9e164627a04505796400c52
Karolina Kozubik <k4aro0@gmail.com>
2024-10-02 20:12:07 +0200

    feat: add new info to schedule

--- src/views/ScheduleView.vue
+++ src/views/ScheduleView.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import StyledHeader from '@/components/styled/StyledHeader.vue'
+import StyledText from '@/components/styled/StyledText.vue'
+</script>
+
 <template>
-  Tu wkrótce pojawi się harmonogram.
+  <StyledHeader>Czas trwania konferencji</StyledHeader>
+  <StyledText> 14 listopada 2024<br />9:00-16:30</StyledText>
+  <StyledText> Więcej szczegółów pojawi się wkrótce. </StyledText>
 </template>
