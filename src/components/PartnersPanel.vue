fa0b9c02a2a4361b457bf8015493618c6a5ce96d
Karolina Kozubik <k4aro0@gmail.com>
2024-10-05 21:07:45 +0200

    fix: add fallback background color

--- src/components/PartnersPanel.vue
+++ src/components/PartnersPanel.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 const sponsors: { path: string; alt: string }[] = [
-  { path: "/partners/POLSAlogotypBLACK.png", alt: "Logotyp POLSA" },
-  { path: "/partners/KP_LABS_LOGO.png", alt: "Logo KP LABS" },
-  { path: "/partners/Nauka_z_Kulturą_LOGO.jpg", alt: "Logo Nauka z Kulturą" },
-  { path: "/partners/SKN_Silesian_Aerospace_Technologies_LOGO.png", alt: "Logo SKN SAT" },
-  { path: "/partners/Logo_astrohunters.png", alt: "Logo Astrohunters" },
+  { path: '/partners/POLSAlogotypBLACK.png', alt: 'Logotyp POLSA' },
+  { path: '/partners/KP_LABS_LOGO.png', alt: 'Logo KP LABS' },
+  { path: '/partners/Nauka_z_Kulturą_LOGO.jpg', alt: 'Logo Nauka z Kulturą' },
+  { path: '/partners/SKN_Silesian_Aerospace_Technologies_LOGO.png', alt: 'Logo SKN SAT' },
+  { path: '/partners/Logo_astrohunters.png', alt: 'Logo Astrohunters' }
 ]
 </script>
 
@@ -13,7 +13,7 @@ const sponsors: { path: string; alt: string }[] = [
     <p class="font-bold text-center text-5xl">Partnerzy wydarzenia</p>
     <div class="pt-16 flex flex-wrap justify-center">
       <div v-for="sponsor in sponsors" :key="sponsors.indexOf(sponsor)">
-        <img class="my-8 md:h-40 md:mx-8" :src="sponsor.path" :alt="sponsor.alt" />
+        <img class="bg-marshland-950 my-8 md:h-40 md:mx-8" :src="sponsor.path" :alt="sponsor.alt" />
       </div>
     </div>
   </div>
