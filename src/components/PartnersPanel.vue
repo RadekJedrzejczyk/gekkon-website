762e03038e38a717a430afee29f11061c9d2dfc8
Karolina Kozubik <k4aro0@gmail.com>
2024-12-18 20:13:22 +0100

    make partners logos go to their websites

--- src/components/PartnersPanel.vue
+++ src/components/PartnersPanel.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-const sponsors: { path: string; alt: string; bg?: string }[] = [
-  { path: '/partners/POLSAlogotypBLACK.png', alt: 'Logotyp POLSA' },
-  { path: '/partners/KP_LABS_LOGO.png', alt: 'Logo KP LABS' },
-  { path: '/partners/Nauka_z_Kulturą_LOGO.jpg', alt: 'Logo Nauka z Kulturą' },
-  { path: '/partners/SKN_Silesian_Aerospace_Technologies_LOGO.png', alt: 'Logo SKN SAT' },
-  { path: '/partners/LogoIFn_v2.png', alt: 'Logo Instytutu Fizyki' },
-  { path: '/partners/Logo_astrohunters.png', alt: 'Logo Astrohunters', bg: 'bg-marshland-950' }
+const sponsors: { path: string; alt: string; src: string; bg?: string }[] = [
+  { path: '/partners/POLSAlogotypBLACK.png', alt: 'Logotyp POLSA', src: 'https://polsa.gov.pl/' },
+  { path: '/partners/KP_LABS_LOGO.png', alt: 'Logo KP LABS', src: 'https://kplabs.space/' },
+  {
+    path: '/partners/Nauka_z_Kulturą_LOGO.jpg',
+    alt: 'Logo Nauka z Kulturą',
+    src: 'https://mrowisko.polsl.pl/blok-kulturalny/nauka-z-kultura/'
+  },
+  {
+    path: '/partners/SKN_Silesian_Aerospace_Technologies_LOGO.png',
+    alt: 'Logo SKN SAT',
+    src: 'https://www.facebook.com/silesianaerospacetechnologiespolsl/'
+  },
+  {
+    path: '/partners/LogoIFn_v2.png',
+    alt: 'Logo Instytutu Fizyki',
+    src: 'https://www.polsl.pl/rif/'
+  },
+  {
+    path: '/partners/Logo_astrohunters.png',
+    alt: 'Logo Astrohunters',
+    src: 'https://www.astrohunters.pl/',
+    bg: 'bg-marshland-950'
+  }
 ]
 </script>
 
@@ -14,11 +31,13 @@ const sponsors: { path: string; alt: string; bg?: string }[] = [
     <p class="font-bold text-center text-5xl">Partnerzy wydarzenia</p>
     <div class="pt-16 flex flex-wrap justify-center">
       <div v-for="sponsor in sponsors" :key="`sponsor-${sponsors.indexOf(sponsor)}`">
-        <img
-          :class="`${sponsor.bg ? sponsor.bg : ''} my-8 max-h-40 md:h-40 md:mx-8`"
-          :src="sponsor.path"
-          :alt="sponsor.alt"
-        />
+        <a :href="sponsor.src" target="_blank">
+          <img
+            :class="`${sponsor.bg ? sponsor.bg : ''} my-8 max-h-40 md:h-40 md:mx-8`"
+            :src="sponsor.path"
+            :alt="sponsor.alt"
+          />
+        </a>
       </div>
     </div>
   </div>
