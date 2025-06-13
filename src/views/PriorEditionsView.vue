0d462949cff02219b2681904f1015a788fe218fe
karofox <k4aro0@gmail.com>
2025-06-13 21:11:42 +0200

    feat: prior editions view routing

---
+++ src/views/PriorEditionsView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import StyledHeader from '@/components/styled/StyledHeader.vue';
+
+const regulationsPDFLink = '/docs/konferencja-gekkon-regulamin.pdf'
+</script>
+
+<template>
+  <StyledHeader>Zapoznaj się z regulaminem konferencji</StyledHeader>
+</template>
