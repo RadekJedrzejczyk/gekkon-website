fc969db999185783e89ea0af208f09f91477daa0
karofox <k4aro0@gmail.com>
2025-06-13 23:20:29 +0200

    feat: fill in prior editions view

--- src/views/PriorEditionsView.vue
+++ src/views/PriorEditionsView.vue
@@ -1,9 +1,81 @@
 <script setup lang="ts">
+import AccentedText from '@/components/styled/AccentedText.vue';
 import StyledHeader from '@/components/styled/StyledHeader.vue';
+import StyledText from '@/components/styled/StyledText.vue';
 
 const regulationsPDFLink = '/docs/konferencja-gekkon-regulamin.pdf'
 </script>
 
 <template>
-  <StyledHeader>Zapoznaj się z regulaminem konferencji</StyledHeader>
+  <StyledHeader>Poprzednie edycje</StyledHeader>
+  <StyledText>
+    <AccentedText>Górnośląska Kosmiczna Konferencja Naukowa "GeKKoN"</AccentedText>, to wydarzenie, które przyciąga
+    pasjonatów kosmosu ze
+    wszelkich dziedzin. Składa sie na nie kilkanaście prelekcji o różnorodnej tematyce od astronomii, przez budownictwo
+    kosmiczne, psychofizjologię w misjach załogowy, aż po inżynierię satelitarną i rakietową.
+    GeKKoN cieszy się wsparciem między innymi <AccentedText>Polskiej Agencji Kosmicznej POLSA</AccentedText> czy gliwickiej firmy sektora kosmicznego <AccentedText>KP
+    Labs</AccentedText>.
+  </StyledText>
+  <div class="mt-8 py-4">
+    <StyledText>
+      <StyledHeader size="2xl">Pierwsza konferencja</StyledHeader>
+
+      Pierwszy GeKKoN odbył się <AccentedText>18 października 2023</AccentedText> roku w <AccentedText>Centrum Kultury Studenckiej "Mrowisko"</AccentedText>. Zorganizowany przez
+      <AccentedText>Studenckie Koło Naukowe Silesian Aerospace Technologies</AccentedText> pod patronatem <AccentedText>Instytutu Roździeńskiego</AccentedText> przyciągnął 150
+      uczestników biernych, którzy zebrali się wysłuchać 13 prelekcji:
+      <ul class="list-disc list-inside py-8">
+        <li>Bogusław Ziębowicz i <AccentedText>kosmiczna muzyka</AccentedText></li>
+        <li><AccentedText>Program ESA BIC</AccentedText></li>
+        <li><AccentedText>Wystąpienie KP Labs</AccentedText></li>
+        <li>Planetarium Śląskie: <AccentedText>Dziwne Przedmioty w Kosmosie</AccentedText></li>
+        <li>Piotr Mendrela: <AccentedText>Praca przeglądowa: powrót na księżyc</AccentedText></li>
+        <li>Jan Czak: <AccentedText>Prawne aspekty eksploracji przestrzeni kosmicznej</AccentedText></li>
+        <li>Dobrochan Fryc: <AccentedText>Badania biomechaniczne w aspekcie psychofizjologii stresu symulowanych misji kosmicznych</AccentedText></li>
+        <li>Zofia Budzik: <AccentedText>Ciekawy przypadek mikrosoczewkowania grawitacyjnego i jego zastosowania</AccentedText></li>
+        <li>Mateusz Smolana, Magdalena Mrozek, Dawid Mrozek: <AccentedText>Prace zespołu SKN "Budownictwo Pozaziemskie" część 1 i 2.</AccentedText></li>
+        <li>Michał Kret, Korneliusz Szalkowski: <AccentedText>Wyzwania związane z opracowaniem studenckego stanowiska testowaego dla rakietowych zespołów.</AccentedText></li>
+        <li>Piotr Jóźwik-Wabik: <AccentedText>Wpływ sieci nauronowych na obesrwacje astronomiczne z powierzchni Ziemi.</AccentedText></li>
+      </ul>
+
+      Wydarzenie objęte było patronatem <AccentedText>Rektora Politechniki Śląskiej</AccentedText>, <AccentedText>Polskiej Agencji Kosmicznej POLSA</AccentedText>, oraz rodzimej
+      firmy satelitarnej <AccentedText>KP Labs</AccentedText>.
+    </StyledText>
+  </div>
+  <div class="mt-8 py-4">
+    <StyledText>
+      <StyledHeader size="2xl">Druga Konferencja</StyledHeader>
+      Druga edycj Górnośląskiej Kosmicznej Konferencji Naukowej GeKKoN tradycyjnie już zagościła w murach <AccentedText>"Mrowiska"</AccentedText> w
+      dniu <AccentedText>14 listopada 2024 roku</AccentedText>. Organizowane przez <AccentedText>AEGEE Gliwice</AccentedText>, z patronatami <AccentedText>Studenckiego Koła Naukowego Silesian
+      Aerospace Technologies</AccentedText>, <AccentedText>Instytutu Fizyki Centrum Naukowo-Dydaktycznego Politechniki Śląskiej</AccentedText>, <AccentedText>Polskiej Agencji
+      Kosmicznej POLSA</AccentedText>, firmy <AccentedText>KP Labs</AccentedText>, <AccentedText>Astrohunters.pl</AccentedText> oraz stowarzyszenia <AccentedText>Nauka z Kulturą</AccentedText> zebrała ponad 130 osób na 13
+      prelekcjach:
+      <ul class="list-disc list-inside py-8">
+        <li>Bogusław Ziębowicz i <AccentedText>kosmiczna muzyka</AccentedText></li>
+        <li>KP Labs o <AccentedText>Intuition-1</AccentedText></li>
+        <li><AccentedText>Astrohunters</AccentedText></li>
+        <li>Jonasz Michalik: <AccentedText>SERNIC - z rodzynkami czy bez?</AccentedText></li>
+        <li>Daniel Marek: <AccentedText>Łaziki Planetarne na Przestrzeni Dekad: Jak AI Zmienia Eksplorację Kosmiczną</AccentedText></li>
+        <li>Błażej Błatoń, Mateusz Warzecha: <AccentedText>Lunamer jako materiał konstrukcyjny bazy księżycowej</AccentedText></li>
+        <li>Michał Wróblewski, Dominik Bereza: <AccentedText>Projekt rakietowego silnika hybrydowego do wyznaczenia parametrów spalania paliw w podtlenku azotu</AccentedText></li>
+        <li>Tobiasz Dąbrowski, Krzysztof Ślubowski: <AccentedText>IKAR, czyli jak stworzyć koło rakietowe w szkole średniej</AccentedText></li>
+        <li>Ignacy Woźniak: <AccentedText>Za Plutona - o podróżach poza Układ Słoneczny</AccentedText></li>
+        <li>Michał Wieczorek: <AccentedText>Metody sztucznej inteligencji w analizie danych hiperspektralnych</AccentedText></li>
+        <li>Zosia Budzik: <AccentedText>Krótka historia eksploracji Marsa</AccentedText></li>
+        <li>Dawid Perzyński: <AccentedText>Kiedy zdobędziemy Układ Słoneczny?</AccentedText></li>
+        <li>Beniamin Kopiec: <AccentedText>Tajemnice Tytana</AccentedText></li>
+      </ul>
+      oraz na sesji posterowej, gdzie pojawiły się nastepujące tematy:
+      <ul class="list-disc list-inside py-8">
+        <li><AccentedText>"ANALIZA SKŁADU ZIARNOWEGO REGOLITU KSIĘŻYCOWEGO Z PRÓBĄ JEGO ODTWORZENIA"</AccentedText></li>
+        <li><AccentedText>"Budowa mikrosatelity z wykorzystaniem mikrokontrolera Arduino"</AccentedText></li>
+        <li><AccentedText>"Długoterminowe loty kosmiczne - wpływ na ośrodkowy układ nerwowy"</AccentedText></li>
+        <li><AccentedText>"ANALIZA MATERIAŁÓW WYKONANIA SILNIKÓW NA PALIWO STAŁE TYPU KARMELEK (R-CANDY)"</AccentedText></li>
+        <li><AccentedText>"ANNOTATING LARGE SATELLITE TELEMETRY DATASET FOR ESA INTERNATIONAL AI ANOMALY DETECTION BENCHMARK"</AccentedText></li>
+        <li><AccentedText>"Adaptacyjne technologie w przestrzeni kosmicznej: Nowe możliwości dla europejskich misji"</AccentedText></li>
+        <li><AccentedText>"A Space Odyssey - Cell culture version. A proposal for a system enabling the study of the impact of cosmic radiation on model cell lines"</AccentedText></li>
+      </ul>
+      Po zakończeniu Konferencje prace posterowe wystawione zostały na terenie <AccentedText>Instytutu Fizyki Centrum
+      Naukowo-Dydaktycznego Politechniki Śląskiej</AccentedText>, a sama transmisja z Konferencji została odtworzona ponad 200 razy.
+    </StyledText>
+  </div>
 </template>
